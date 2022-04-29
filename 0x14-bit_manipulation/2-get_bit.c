312298ec99d411697d32cfa6dcb949fdff6dcbd6
TonyGash <tonygaya5@gmail.com>
2022-04-30 01:18:24 +0300

    manipulate

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * BYTE_LENGTH)
+	if (index >= sizeof(n) * BYTE_LENGHT)
 		return (-1);
 
 	return ((n >> index) & 1);
